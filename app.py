c95442cb11942f4c20b82288fa16f9cc79d4fa2a
Rsousa1213 <9g67hg7t84@privaterelay.appleid.com>
2026-09-16 19:39:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -106,9 +106,9 @@ LEAGUES = {
         "url_prev": "https://www.football-data.co.uk/mmz4281/2526/E0.csv",
         "api_key": "soccer_epl"
     },
- "BR Brasileirão Série A": {
-        "url": "https://www.football-data.co.uk/new_league_data/BRA.csv",
-        "url_prev": "https://www.football-data.co.uk/new_league_data/BRA.csv",
+    "BR Brasileirão Série A": {
+        "url": "https://www.football-data.co.uk/new/BRA.csv",
+        "url_prev": "https://www.football-data.co.uk/new/BRA.csv",
         "api_key": "soccer_brazil_campeonato"
     },
     "EU Liga dos Campeões": {
@@ -126,9 +126,9 @@ LEAGUES = {
         "url_prev": "https://www.football-data.co.uk/mmz4281/2526/N1.csv",
         "api_key": "soccer_netherlands_eerste_divisie"
     },
-  "AR Liga Profesional (Argentina)": {
-        "url": "https://www.football-data.co.uk/new_league_data/ARG.csv",
-        "url_prev": "https://www.football-data.co.uk/new_league_data/ARG.csv",
+    "AR Liga Profesional (Argentina)": {
+        "url": "https://www.football-data.co.uk/new/ARG.csv",
+        "url_prev": "https://www.football-data.co.uk/new/ARG.csv",
         "api_key": "soccer_argentina_primera_division"
     },
 }
