f71fbaf74352d5079ffb6d12246b31b4938f0e7a
Rsousa1213 <9g67hg7t84@privaterelay.appleid.com>
2026-09-17 21:51:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ aplicar_estilo_visual()
 st.title("⚽ Análise Desportiva 26/27")
 st.markdown("Análise Avançada com Foco em **Over 1.5 & Over 2.5 Golos** & **Over 7.5 Cantos**")
 
-# 2. Dicionário de Ligas com Equipas Reais (Com Ligue 1 Francesa incluída)
+# 2. Dicionário de Ligas com Equipas Reais (Com Bundesliga Alemã incluída)
 LEAGUES_TEAMS = {
     "🏆 Copa Libertadores": [
         "Flamengo", "Palmeiras", "Atlético Mineiro", "Fluminense", "São Paulo", 
@@ -86,6 +86,11 @@ LEAGUES_TEAMS = {
         "Manchester City", "Arsenal", "Liverpool", "Aston Villa", "Tottenham", 
         "Chelsea", "Newcastle", "Manchester United", "West Ham", "Crystal Palace", 
         "Brighton", "Bournemouth", "Fulham", "Wolves", "Everton", "Brentford", "Nottingham Forest", "Leicester City", "Ipswich Town", "Southampton"
+    ],
+    "DE Bundesliga (Alemanha)": [
+        "Bayern München", "Bayer Leverkusen", "Borussia Dortmund", "RB Leipzig", "Stuttgart", 
+        "Eintracht Frankfurt", "Wolfsburg", "Hoffenheim", "Freiburg", "Werder Bremen", 
+        "Union Berlin", "Augsburg", "Mainz", "Borussia Mönchengladbach", "Bochum", "Heidenheim", "St. Pauli", "Holstein Kiel"
     ],
     "FR Ligue 1 (França)": [
         "PSG", "Monaco", "Marseille", "Lille", "Lyon", 
