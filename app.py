b176dae48b261b3a1f87d7553d35c21f8c47bc86
Rsousa1213 <9g67hg7t84@privaterelay.appleid.com>
2026-09-17 12:36:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,6 @@ LEAGUES = {
     "BR Brasileirão (Brasil)": {"url": "https://www.football-data.co.uk/new/BRA.csv", "type": "domestic"},
     "AR Liga Profesional (Argentina)": {"url": "https://www.football-data.co.uk/new/ARG.csv", "type": "domestic"}
 }
-}
 
 TEAMS_CL_2627 = [
     "Real Madrid", "Barcelona", "Manchester City", "Arsenal", "Bayern München", 
