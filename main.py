babfe3537fb0833496790f188b13fb17226f349c
Rsousa1213 <9g67hg7t84@privaterelay.appleid.com>
2026-09-20 19:31:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -416,8 +416,11 @@ odd_btts = st.sidebar.number_input(
 odd_cantos_over_75 = st.sidebar.number_input(
     "Odd Cantos Over 7.5", min_value=1.01, value=1.55, step=0.01
 )
-odd_cartoes_over_25 = st.sidebar.number_input(
-    "Odd Cartões Over 2.5", min_value=1.01, value=1.70, step=0.01
+odd_cartoes_over_45 = st.sidebar.number_input(
+    "Odd Cartões Over 4.5", min_value=1.01, value=1.90, step=0.01
+)
+odd_golo_1p = st.sidebar.number_input(
+    "Odd Marcar na 1ª Parte", min_value=1.01, value=1.40, step=0.01
 )
 
 # 3. Corpo Principal - Análise de Jogos
@@ -512,7 +515,11 @@ else:
       else 2.1
   )
   lambda_total_cartoes = media_hy + media_ay + 0.5
-  prob_cartoes_over_25 = 1 - poisson.cdf(2, lambda_total_cartoes)
+  prob_cartoes_over_45 = 1 - poisson.cdf(4, lambda_total_cartoes)
+
+  # Probabilidade de golo na 1ª parte (aproximadamente 45% dos golos totais ocorrem na 1ª parte)
+  lambda_1p = (lambda_home + lambda_away) * 0.45
+  prob_golo_1p = 1 - poisson.pmf(0, lambda_1p)
 
   max_goals = 6
   matriz_prob = np.outer(
@@ -571,9 +578,14 @@ else:
           "Odd_Manual": odd_cantos_over_75,
       },
       {
-          "Mercado": "Cartões Over 2.5",
-          "Prob_Calc": prob_cartoes_over_25,
-          "Odd_Manual": odd_cartoes_over_25,
+          "Mercado": "Cartões Over 4.5",
+          "Prob_Calc": prob_cartoes_over_45,
+          "Odd_Manual": odd_cartoes_over_45,
+      },
+      {
+          "Mercado": "Marcar na 1ª Parte",
+          "Prob_Calc": prob_golo_1p,
+          "Odd_Manual": odd_golo_1p,
       },
   ]
 
